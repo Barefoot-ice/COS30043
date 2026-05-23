c61ca6685761293190fc374ac116e0f83a17e457
alex10342 <60557419+alex10342@users.noreply.github.com>
2026-05-23 16:29:55 +1000

    api

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <script>
 import { mapState } from "vuex";
-import sqlBackend from "./lib/sqlBackend";
+import { api } from "./lib/api";
 
-sqlBacked("SELECT * FROM city");
+console.log(await api.getJobs().then(res => res.json()))
+
 
 export default {
     data: function () {
@@ -25,18 +26,10 @@ export default {
 <template>
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
         <div class="container">
-            <router-link class="navbar-brand" to="/"
-                >COS30043 Group Project</router-link
-            >
-            <button
-                class="navbar-toggler"
-                type="button"
-                data-bs-toggle="collapse"
-                data-bs-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent"
-                aria-expanded="false"
-                aria-label="Toggle navigation"
-            >
+            <router-link class="navbar-brand" to="/">COS30043 Group Project</router-link>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
@@ -45,37 +38,22 @@ export default {
                         <router-link class="nav-link" to="/">Home</router-link>
                     </li>
                     <li class="nav-item">
-                        <router-link class="nav-link" to="/FAQ"
-                            >FAQ</router-link
-                        >
+                        <router-link class="nav-link" to="/FAQ">FAQ</router-link>
                     </li>
                     <li class="nav-item">
-                        <router-link class="nav-link" to="/signup"
-                            >Sign Up</router-link
-                        >
+                        <router-link class="nav-link" to="/signup">Sign Up</router-link>
                     </li>
                 </ul>
                 <div v-if="loggedIn">
-                    <span class="navbar-text"
-                        >Welcome, {{ user.firstName }}!</span
-                    >
+                    <span class="navbar-text">Welcome, {{ user.firstName }}!</span>
                     <button class="btn btn-primary" @click="submitLogOut">
                         Logout
                     </button>
                 </div>
                 <div v-else>
                     <form class="d-flex">
-                        <input
-                            type="text"
-                            class="form-control"
-                            placeholder="Username"
-                            v-model="nameInput"
-                        />
-                        <input
-                            type="text"
-                            class="form-control"
-                            placeholder="Password"
-                        />
+                        <input type="text" class="form-control" placeholder="Username" v-model="nameInput" />
+                        <input type="text" class="form-control" placeholder="Password" />
                         <button class="btn btn-primary" @click="submitLogIn">
                             Login
                         </button>
