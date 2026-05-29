1e01a7f01e00083bbfbfbe72d4f1ae6c3e3e8c35
Barefoot-ice <163299185+Barefoot-ice@users.noreply.github.com>
2026-05-30 01:36:57 +1000

    Update Account.vue
    multi form functionality for acc page still missing:
    form validation,
    Put call to edit database value

--- src/components/Account.vue
+++ src/components/Account.vue
@@ -1,14 +1,78 @@
 <template>
 <div class='container'>
 <h3>Account Details</h3>
-<div>
-    <!-- static account details pag
-    todo: and an on button event to change usrname, pass and email fields to changeable and use same controls as signup then send put req on new button that appears after changing to edit, add a cancel changes button too-->
-    <p>Account Username: {{ details.username }}</p>
-    <p>Account Email Address: {{ details.email }}</p>
-    <p>Account Password: {{ details.password }}</p>
-    <p>Date of account creation: {{ details.doc }}</p>
-    <p>Account permissions: {{ details.role }}</p>
+ <!-- Page when dets loaded-->
+<div v-if="details.username && !isEditing">
+    <v-text-field
+        label="Account Username"
+        v-model="details.username"
+        readonly
+    ></v-text-field>
+    <v-text-field
+        label="Account Email Address"
+        v-model="details.email"
+        readonly
+    ></v-text-field>
+    <v-text-field
+        label="Account Password"
+        v-model="details.password"
+        type="password"
+        readonly
+    ></v-text-field>
+     <v-text-field
+        label="Time of Account creation"
+        v-model="details.doc"
+        readonly
+    ></v-text-field>
+    <v-text-field
+        label="Account permissions"
+        v-model="details.role"
+        readonly
+    ></v-text-field>
+    <div class ="text-end">
+        <button type="button" class="btn btn-primary" @click="isEditing = true">Change Details</button>
+    </div>
+</div>
+ <!-- Page when editting dets, no functionality yet-->
+<div v-if="details.username && isEditing">
+    <v-text-field
+        label="Account Username"
+        v-model="editedDetails.username"
+        
+    ></v-text-field>
+    <v-text-field
+        label="Account Email Address"
+        v-model="editedDetails.email"
+        
+    ></v-text-field>
+    <v-text-field
+        label="Account Password"
+        v-model="editedDetails.password"
+        type="password"
+    ></v-text-field>
+     <v-text-field
+        label="Time of Account creation"
+        v-model="details.doc"
+        readonly
+    ></v-text-field>
+    <v-text-field
+        label="Account permissions"
+        v-model="details.role"
+        readonly
+    ></v-text-field>
+    <div>
+        <div class ="text-start">
+            <button type="button" class="btn btn-warning" @click="cancelEdit">Revert changes</button>
+        </div>
+        <div class ="text-end">
+            <button type="button" class="btn btn-success" @click="saveDetails">Success</button>
+        </div>
+    </div>
+
+</div>
+ <!-- Page when dets unloaded-->
+<div v-if="!details.username">
+  <p>Account details currently not available, please refresh page</p>
 </div>
 </div>
 </template>
@@ -20,6 +84,8 @@ export default {
     data() {
         return {
             details: {}, 
+            editedDetails: {}, 
+            isEditing: false,
         };
     },
     async mounted() {
@@ -29,5 +95,20 @@ export default {
             console.error("Failed to load user:", err);
         }
     },
+    methods: {
+        cancelEdit() {
+            this.isEditing = false;
+        },
+        saveDetails() {
+      // TODO: Call your update API here after adding form controls
+            this.details = { ...this.editedDetails }; 
+            this.isEditing = false;
+        }
+    },
+    watch: {
+        details(newVal) {
+            this.editedDetails = { ...newVal };
+        }
+  }
 };
 </script>
