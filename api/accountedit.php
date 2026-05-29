1b964954c24b2a067ef4d7e9630342525be3f5c3
Barefoot-ice <163299185+Barefoot-ice@users.noreply.github.com>
2026-05-29 22:42:43 +1000

    Update accountedit.php
    account edit php and function complete

--- api/accountedit.php
+++ api/accountedit.php
@@ -1,10 +1,7 @@
 <?php
 http_response_code(400);
 //still in progress
-$id = $_GET['id'];
-$username = $_GET['username'];
-$pass = $_GET['pass'];
-$email = $_GET['email'];
+
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     exit(0);
@@ -14,12 +11,18 @@ $method = $_SERVER['REQUEST_METHOD'];
 
 require 'sql.php';
 
+if ($method === 'PUT') {
+    $id = $_GET['id'];
+    $json = file_get_contents('php://input');
+    $params = json_decode($json, true);
+    $username = isset($params['email']) ? trim($params['email']) : '';
+    $username = isset($params['username']) ? trim($params['username']) : '';
+    $password = isset($params['password']) ? trim($params['password']) : '';
+    $query = "UPDATE `accounts` SET username = $username, email = $email, password = $password WHERE id=$id;"
+    $result = mysqli_query($conn, $query);
 
-$query = "UPDATE `accounts` SET username = $username, email = $email, password = $pass WHERE id=$id";;
-$result = mysqli_query($conn, $query);
-
-$data = mysqli_fetch_all($result);
-http_response_code(200);
-echo json_encode($data);
+    $data = mysqli_fetch_all($result);
+    http_response_code(200);
+    echo json_encode($data);
 
 ?>
