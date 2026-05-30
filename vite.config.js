6b1977ecab5b0b40d474554c1213ed90b52e0b4f
Zehpy86 <166472042+Zehpy86@users.noreply.github.com>
2026-05-30 20:03:23 +1000

    dave

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vuetify from 'vite-plugin-vuetify';
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [vue(), vuetify({ autoImport: true })],
-});
+    base: mode === 'production' ? '/cos30043/s105139158/assignment/' : '/',
+}))
