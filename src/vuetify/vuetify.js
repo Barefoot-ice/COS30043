5f2407186d057b9ca41b2425f8cabfb55f30fdf3
Barefoot-ice <163299185+Barefoot-ice@users.noreply.github.com>
2026-05-30 18:30:41 +1000

    Add files via upload

---
+++ src/vuetify/vuetify.js
@@ -0,0 +1,7 @@
+import '@mdi/font/css/materialdesignicons.css' // Import icons
+import 'vuetify/styles' // Import structural styles
+import { createVuetify } from 'vuetify'
+
+export default createVuetify({
+
+})
