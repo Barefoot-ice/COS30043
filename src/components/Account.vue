c80b861a7e3732bc3aba1d0389eccbc11b0b861b
Barefoot-ice <163299185+Barefoot-ice@users.noreply.github.com>
2026-05-30 16:10:38 +1000

    Update Account.vue
    account page updated with get functionality 
    put and form controls still pending

--- src/components/Account.vue
+++ src/components/Account.vue
@@ -1,77 +1,74 @@
 <template>
-<div class='container'>
-<h3>Account Details</h3>
+<div class='container mt-4'>
+
  <!-- Page when dets loaded-->
+ <!-- View Mode -->
 <div v-if="details.username && !isEditing">
-    <v-text-field
-        label="Account Username"
-        v-model="details.username"
-        readonly
-    ></v-text-field>
-    <v-text-field
-        label="Account Email Address"
-        v-model="details.email"
-        readonly
-    ></v-text-field>
-    <v-text-field
-        label="Account Password"
-        v-model="details.password"
-        type="password"
-        readonly
-    ></v-text-field>
-     <v-text-field
-        label="Time of Account creation"
-        v-model="details.doc"
-        readonly
-    ></v-text-field>
-    <v-text-field
-        label="Account permissions"
-        v-model="details.role"
-        readonly
-    ></v-text-field>
-    <div class ="text-end">
-        <button type="button" class="btn btn-primary" @click="isEditing = true">Change Details</button>
+    <h3 class="text-center mb-4 fw-bold">Account Details</h3>
+    <div class="row g-3">
+    <div class="col-12 col-md-6 col-lg-4">
+        <label class="form-label fw-bold">Account Username</label>
+        <input type="text" class="form-control" v-model="details.username" readonly>
+    </div>
+    <div class="col-12 col-md-6 col-lg-4">
+        <label class="form-label fw-bold">Account Email Address</label>
+        <input type="email" class="form-control" v-model="details.email" readonly>
+    </div>
+    <div class="col-12 col-md-6 col-lg-4">
+        <label class="form-label fw-bold">Account Password</label>
+        <input type="password" class="form-control" v-model="details.password" readonly>
+    </div>
+    <div class="col-12 col-md-6 col-lg-4">
+        <label class="form-label fw-bold">Time of Account creation</label>
+        <input type="text" class="form-control" v-model="details.created_at" readonly>
+    </div>
+    <div class="col-12 col-md-6 col-lg-4">
+        <label class="form-label fw-bold">Account permissions</label>
+        <input type="text" class="form-control" v-model="details.role" readonly>
+    </div>
+    <div class="col-12 text-end mt-4">
+        <button type="button" class="btn btn-primary" @click="startEdit">Change Details</button>
+    </div>
     </div>
 </div>
- <!-- Page when editting dets, no functionality yet-->
-<div v-if="details.username && isEditing">
-    <v-text-field
-        label="Account Username"
-        v-model="editedDetails.username"
-        
-    ></v-text-field>
-    <v-text-field
-        label="Account Email Address"
-        v-model="editedDetails.email"
-        
-    ></v-text-field>
-    <v-text-field
-        label="Account Password"
-        v-model="editedDetails.password"
-        type="password"
-    ></v-text-field>
-     <v-text-field
-        label="Time of Account creation"
-        v-model="details.doc"
-        readonly
-    ></v-text-field>
-    <v-text-field
-        label="Account permissions"
-        v-model="details.role"
-        readonly
-    ></v-text-field>
-    <div>
-        <div class ="text-start">
-            <button type="button" class="btn btn-warning" @click="cancelEdit">Revert changes</button>
-        </div>
-        <div class ="text-end">
-            <button type="button" class="btn btn-success" @click="saveDetails">Success</button>
-        </div>
-    </div>
 
+ <!-- Edit Mode -->
+<div v-if="details.username && isEditing">
+    <h3 class="text-center mb-4 fw-bold">Account Details</h3>
+    <div class="row g-3">
+    <div class="col-12 col-md-6 col-lg-4">
+        <label class="form-label fw-bold">Account Username</label>
+        <input type="text" class="form-control" v-model="editedDetails.username">
+    </div>
+    <div class="col-12 col-md-6 col-lg-4">
+        <label class="form-label fw-bold">Account Email Address</label>
+        <input type="email" class="form-control" v-model="editedDetails.email">
+    </div>
+    <div class="col-12 col-md-6 col-lg-4">
+        <label class="form-label fw-bold">Account Password</label>
+        <input type="password" class="form-control" v-model="editedDetails.password">
+    </div>
+    <div class="col-12 col-md-6 col-lg-4">
+        <label class="form-label fw-bold">Confirm Password</label>
+        <input type="password" class="form-control" v-model="confirmationPassword">
+    </div>
+    <div class="col-12 col-md-6 col-lg-4">
+        <label class="form-label fw-bold">Time of Account creation</label>
+        <input type="text" class="form-control" v-model="details.doc" readonly>
+    </div>
+    <div class="col-12 col-md-6 col-lg-4">
+        <label class="form-label fw-bold">Account permissions</label>
+        <input type="text" class="form-control" v-model="details.role" readonly>
+    </div>
+    <div class="col-12 d-flex justify-content-between mt-4">
+        <button type="button" class="btn btn-warning" @click="cancelEdit">Revert changes</button>
+        <button type="button" class="btn btn-success" @click="saveDetails">Success</button>
+    </div>
+    </div>
 </div>
  <!-- Page when dets unloaded-->
 <div v-if="!details.username">
+    <h3 class="text-center mb-4 fw-bold">Account Details</h3>
   <p>Account details currently not available, please refresh page</p>
 </div>
 </div>
@@ -81,34 +78,54 @@
 import { api } from "../lib/api";
 
 export default {
+
     data() {
         return {
             details: {}, 
             editedDetails: {}, 
+            confirmationPassword: "",
             isEditing: false,
         };
     },
+    computed: {
+        currentUserId() {
+            return this.$store.getters.userId;
+        }
+    },
     async mounted() {
         try {
-            this.details = await api.accountget();
+            if (this.currentUserId) {
+                this.details = await api.getAccount(this.currentUserId);
+                console.log(this.details.created_at)
+            } else {
+                console.warn("No user is currently logged in.");
+                this.$router.push('/'); 
+            }
         } catch (err) {
             console.error("Failed to load user:", err);
         }
     },
     methods: {
+        startEdit() {
+
+            this.editedDetails = { ...this.details };
+            this.confirmationPassword = this.details.password;
+            this.isEditing = true;
+        },
         cancelEdit() {
             this.isEditing = false;
         },
         saveDetails() {
-      // TODO: Call your update API here after adding form controls
+            if (this.editedDetails.password !== this.confirmationPassword) {
+                alert("Passwords do not match!");
+                return;
+            }
+            //call api and update vuex
             this.details = { ...this.editedDetails }; 
             this.isEditing = false;
-        }
-    },
-    watch: {
-        details(newVal) {
-            this.editedDetails = { ...newVal };
-        }
-  }
+        },
+    }
+
+    
 };
 </script>
