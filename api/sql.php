5004fb68289248477db6b64d654908877fb12242
Michael <michaelattardi47@gmail.com>
2026-05-30 23:53:56 +1000

    Reverted mercury credentials to Dave's

--- api/sql.php
+++ api/sql.php
@@ -1,8 +1,8 @@
 <?php
 $host = "feenix-mariadb.swin.edu.au";
-$user = "s102096755";
-$pwd = "102096755COS10032";
-$sql_db = "s102096755_db";
+$user = "s105338913";
+$pwd = "280905";
+$sql_db = "s105338913_db";
 
 $conn = @mysqli_connect($host, $user, $pwd, $sql_db);
 ?>
--- src/lib/api.js
+++ src/lib/api.js
@@ -1,4 +1,4 @@
-const BASE = "https://mercury.swin.edu.au/cos30043/s102096755/project/api";
+const BASE = "https://mercury.swin.edu.au/cos30043/s105139158/assignment/api";
 
 const mapArrayToJob = (row) => {
   return {
