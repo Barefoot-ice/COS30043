792976c2f267342ed7a78dcaadc67e797031ca00
CharlieMurdoch1 <103988219@student.swin.edu.au>
2026-05-30 18:04:44 +1000

    Social feed added to navigation

--- src/App.vue
+++ src/App.vue
@@ -56,6 +56,9 @@ export default {
                     <li class="nav-item">
                         <router-link class="nav-link" to="/about">About</router-link>
                     </li>
+                    <li class="nav-item">
+                        <router-link class="nav-link" to="/social">Social</router-link>
+                    </li>
                     <li v-if="loggedIn === false" class="nav-item">
                         <router-link class="nav-link" to="/signup">Sign Up</router-link>
                     </li>
