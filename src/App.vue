1d27c10add55be84ab7b27de47729a14e00879b8
Zehpy86 <166472042+Zehpy86@users.noreply.github.com>
2026-05-30 18:27:46 +1000

    Nav to social-post

--- src/App.vue
+++ src/App.vue
@@ -36,13 +36,13 @@ export default {
 <template>
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
         <div class="container">
-            <router-link class="navbar-brand" to="/">COS30043 Group Project</router-link>
+            <router-link class="navbar-brand justify-content-start" to="/">COS30043 Group Project</router-link>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                 data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
                 aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
+            <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
                 <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                     <li class="nav-item">
                         <router-link class="nav-link" to="/">Home</router-link>
@@ -59,24 +59,48 @@ export default {
                     <li v-if="loggedIn === false" class="nav-item">
                         <router-link class="nav-link" to="/signup">Sign Up</router-link>
                     </li>
-                    <li v-if="user.role === 'admin'" class="nav-item">
-                        <router-link class="nav-link" to="/admin">Approve Job Posting</router-link>
-                    </li>
                 </ul>
-                <div v-if="loggedIn">
+                <template v-if="loggedIn">
                     <span class="navbar-text">Welcome, {{ user.username }}!</span>
                     <button class="btn btn-primary" @click="submitLogOut">
                         Logout
                     </button>
-                </div>
-                <div v-else>
+                </template>
+                <template v-else>
                     <form class="d-flex" @submit.prevent="submitLogIn($refs.username.value, $refs.password.value)">
                         <input type="text" ref="username" class="form-control" placeholder="Username" />
                         <input type="password" ref="password" class="form-control" placeholder="Password" />
                         <button type="submit" class="btn btn-primary">Login</button>
                     </form>
                     <p class="text-align-end" v-if="loginErr">{{ loginErr }}</p>
+                </template>
+            <template v-if="loggedIn">
+                <div class="dropdown">
+                    <button 
+                        class="btn btn-primary dropdown-toggle" 
+                        type="button" 
+                        id="dropdownMenuButton" 
+                        data-bs-toggle="dropdown" 
+                        aria-expanded="false"
+                    >
+                        What would you like to do today?
+                    </button>
+                    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                        <li v-if="user.role === 'admin'" class="dropdown-header" >
+                            ---- ADMIN -----
+                        </li>
+                        <li v-if="user.role === 'admin'" class="dropdown-item">
+                            <router-link class="nav-link" to="/admin">Approve Job Posting</router-link>
+                        </li>
+                        <li class="dropdown-header">
+                            ---- USER ----
+                        </li>
+                        <li class="dropdown-item">
+                            <router-link class="nav-link" to="/social">Post to Social</router-link>
+                        </li>
+                    </ul>
                 </div>
+            </template>
             </div>
         </div>
     </nav>
