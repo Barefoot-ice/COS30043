fe58d8cea8cccc9d992bbf32aa2b8a9c3ae64bb0
Zehpy86 <166472042+Zehpy86@users.noreply.github.com>
2026-05-30 20:09:09 +1000

    Mini nav fix

--- src/App.vue
+++ src/App.vue
@@ -66,14 +66,8 @@ export default {
                     <li class="nav-item">
                         <router-link class="nav-link" to="/about">About</router-link>
                     </li>
-                    <li class="nav-item">
-                        <router-link class="nav-link" to="/social">Social</router-link>
-                    </li>
                     <li v-if="loggedIn === false" class="nav-item">
                         <router-link class="nav-link" to="/signup">Sign Up</router-link>
-                    </li>
-                    <li v-if="loggedIn === true" class="nav-item">
-                        <router-link class="nav-link" to="/account">Account Details</router-link>
                     </li>
                 </ul>
                 <template v-if="loggedIn">
@@ -113,6 +107,9 @@ export default {
                         </li>
                         <li class="dropdown-item">
                             <router-link class="nav-link" to="/social">Post to Social</router-link>
+                        </li>
+                        <li class="dropdown-item">
+                            <router-link class="nav-link" to="/account">Account Details</router-link>
                         </li>
                     </ul>
                 </div>
